1a3c0dde3bec6929508b6e9b336a144d582a8e73
keysi88 <140400343+keysi88@users.noreply.github.com>
2023-11-21 15:50:03 +0300

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,113 @@
+
+// // Задание 1
+// let password = ('пароль');
+// let request = (prompt('Введите пароль'));
+// if (request === password) {
+// alert('Пароль введен верно'); 
+    
+// } else {
+    // alert('Пароль введен неверно');
+    
+// }
+
+// // Задание 2
+// let c = 5;
+// if (c > 0 && c < 10) {
+//   console.log ('Верно');
+   
+// } else {
+//   console.log('Неверно');
+   
+// }
+
+// //Задание 3
+// let d = 300;
+// let e = 70;
+// if (d > 100 || e > 100) {
+//   console.log('Верно');
+   
+// } else {
+//   console.log('Неверно');
+   
+// }
+
+// // Задание 4
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+
+// // Задание 5 
+// let monthNumber = 12;
+
+// switch (monthNumber) {
+//   case 1:
+//   case 2:
+//   case 12:
+//      break;
+//   case 3:
+//   case 4:
+//   case 5:
+//      alert('Весна');
+//      break;
+//   case 6:
+//   case 7:
+//   case 8:
+//      alert('Лето');
+//      break;
+//   case 9:
+//   case 10:
+//   case 11:
+//      alert('Осень');
+//      break;
+//   default:
+//      alert('Неизвестный месяц');
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
